c8d205aef0f57dd7645e28ccbd1d3e9944c920be
Nicolas Diez <nicolas439@gmail.com>
2022-04-03 12:15:52 +0200

    testing some ideas related to proof of stake, mining and conflict solving

--- blockchain_node/blockchain_node.py
+++ blockchain_node/blockchain_node.py
@@ -343,3 +343,80 @@ if __name__ == '__main__':
     port = args.port
 
     app.run(host='127.0.0.1', port=port, debug=True)
+
+
+
+
+
+
+# -----------------------------------------------------------------------------------------------
+# --------------------------------- TESTING SOME FUNCTIONALITIES --------------------------------
+
+
+    def proof_of_stake(self):
+        # get the last block
+        last_block = self.chain[-1]
+        # hash the last block
+        last_hash = self.hash(last_block)
+        # try nonce from 0, until we get a nonce that meet the DIFFICULTY criteria (eg: 2 leading zeros in the hash)
+        nonce = 0
+        while self.valid_proof(self.transactions, last_hash, nonce) is False:
+            nonce += 1
+        return nonce
+
+
+def mine_2():
+    # run the proof of work algorithm
+    nonce = blockchain.proof_of_work()
+
+    # reward the miner = submit a new transaction to be incorporated into the next block
+    blockchain.submit_transaction(sender_public_key = MINING_SENDER,
+                                  recipient_public_key = blockchain.node_id,
+                                  signature = '',   # when rewarding the miner, there is no signature
+                                  amount = MINING_REWARD)
+
+    last_block = blockchain.chain[-1]  # get the last block of the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.create_block(nonce = nonce, previous_hash = previous_hash)
+
+    response = {
+        'message': 'New block created',
+        'block_number': block['block_number'],
+        'transactions': block['transactions'],
+        'nonce': block['nonce'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+
+def resolve_conflicts_2(self):
+
+    # every node maintains a list with the rest of the nodes available in the network (that list is in self.nodes)
+    neighbours = self.nodes
+    new_chain = None
+
+    # length of the existing chain in the node
+    self_node_chain_length = len(self.chain)
+
+    for node in neighbours:
+
+        # get the latest version of the chain from the neighbour node
+        response = requests.get('http://' + node + '/chain')
+
+        if response.status_code == 200:
+            neighbour_node_chain_length = response.json()['length']
+            neighbour_chain = response.json()['chain']
+
+            # if the neighbour node has a longer chain than the self node chain, and his chain is valid, then
+            # the self node has to replace his chain
+            if neighbour_node_chain_length > self_node_chain_length and self.valid_chain(neighbour_chain):
+                self_node_chain_length = neighbour_node_chain_length
+                new_chain = neighbour_chain
+
+    # new_chain holds the longest valid chain among all the neighbour nodes
+    # then update my self chain with the new_chain
+    if new_chain:
+        self.chain = new_chain
+        return True
+
+    return False
